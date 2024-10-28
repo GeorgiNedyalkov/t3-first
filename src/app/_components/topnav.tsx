2a42517b702cede031dabd938a3bdece6f30c2ed
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-29 00:13:04 +0200

    working on parallel routes

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { SignedIn, SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
@@ -8,7 +9,9 @@ export default function TopNav() {
   const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+      <Link href="/">
+        <div className="cursor-pointer">Gallery</div>
+      </Link>
       <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getImages } from "~/server/db/queries";
@@ -9,14 +10,16 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
         <div key={image.id} className="h-64 w-64">
-          <Image
-            src={image.url}
-            alt=""
-            width={480}
-            height={480}
-            style={{ objectFit: "contain" }}
-          />
-          <div>{image.name}</div>
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              alt=""
+              width={480}
+              height={480}
+              style={{ objectFit: "contain" }}
+            />
+            <div>{image.name}</div>
+          </Link>
         </div>
       ))}
     </div>
