0375441c72fd9dd693d6ccea714753c07fc645d5
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-28 21:16:19 +0200

    update order for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,15 @@
 import { db } from "~/server/db";
 
-const mockData = [
-  "https://utfs.io/f/AZ4TpV2wRXr39vW2ItrCSn6xNMjXbH5r3lFuyJQvZDqR12hL",
-  "https://utfs.io/f/AZ4TpV2wRXr3sbFkQH4dUM8CYH5vt9EfyDSob1TrAOpm2zig",
-  "https://utfs.io/f/AZ4TpV2wRXr3ZSKiItc730B2KVGIj5RnWma6vhzyeSFAJHtg",
-  "https://utfs.io/f/AZ4TpV2wRXr3W0Vg13DH4EBuD2JZjX85vHMT1kiozAYghsUt",
-  "https://utfs.io/f/AZ4TpV2wRXr3RZnb4bVahBOP1UH4Lz92A7jwDxoNrXm0VQkE",
-];
-
-const mockImages = mockData.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
+        {images.map((image) => (
+          <div key={image.id} className="w-64">
             <img src={image.url} alt="image" />
           </div>
         ))}
