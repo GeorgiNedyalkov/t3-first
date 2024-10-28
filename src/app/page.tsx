59215f209b4bd49be1b1d43bc5d1d83b15811e6d
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-28 21:40:09 +0200

    add singedin and signout logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,33 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="w-64">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={image.url} alt="image" />
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="w-64">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={image.url} alt="image" />
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please Sign In above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
