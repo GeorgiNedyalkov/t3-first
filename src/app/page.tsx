5aab8507d3ed83dff035a61fb2d5e3e5e015b83c
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-28 20:57:02 +0200

    connect to DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockData = [
   "https://utfs.io/f/AZ4TpV2wRXr39vW2ItrCSn6xNMjXbH5r3lFuyJQvZDqR12hL",
   "https://utfs.io/f/AZ4TpV2wRXr3sbFkQH4dUM8CYH5vt9EfyDSob1TrAOpm2zig",
@@ -11,12 +13,19 @@ const mockImages = mockData.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src={image.url} alt="image" />
           </div>
